f70c73da6e98e77d605d8510dfdb6da29ae85a38
M. Jameel Baig <mjameelbaig@hotmail.com>
2020-05-15 02:49:25 +1000

    with tabs

--- Screens/TabsForWeather/UvTab.js
+++ Screens/TabsForWeather/UvTab.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
+import axios from "axios";
 
 export default class UvTab extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            'currindex': null,
+            'forcast': null
+        }
+    }
+    componentDidMount(){
+        let today = new Date()
+        let myString = `${today.getDate()-1}-${today.getMonth()+1}-${today.getFullYear()}`;
+        axios.get(`https://dire-api.herokuapp.com/?date=${myString}`).then(res => {
+            console.log(res.data.current);
+            this.setState({
+                'currindex': res.data.current.CurrentUVIndex,
+                'forecast': res.data.forecast
+            })
+
+            console.log(this.state)
+        }, err => {
+            console.log(err);
+        }, () => {
+            console.log("DOne");
+        })
+    }
     render() {
+        let { currindex } = this.state;
         return (
             <View>
-                <Text> textInComponent </Text>
+                {
+                    currindex && 
+                    <Text>{currindex}</Text>
+                }
             </View>
         )
     }
--- Screens/WeatherScreen.js
+++ Screens/WeatherScreen.js
@@ -6,29 +6,33 @@ import Tab2 from './TabsForWeather/UvTab';
 import Tab3 from './TabsForWeather/RainTab'
 
 export default class NewScreen extends Component {
-    render() {
-        return (
-          <Container tabStyle={styles.contaierStyle} >
-            <Tabs tabBarUnderlineStyle={{backgroundColor:"white"}} >
-              <Tab tabStyle={{backgroundColor:"#F3BA36"}} activeTabStyle={{backgroundColor:"#F3BA36"}} 
-              textStyle={{color:"black"}} activeTextStyle={{color:"white"}} heading="Temprature">
-                <Tab1 />
-              </Tab>
-              <Tab tabStyle={{backgroundColor:"#F3BA36"}} activeTabStyle={{backgroundColor:"#F3BA36"}} 
-              textStyle={{color:"black"}} activeTextStyle={{color:"white"}} heading="UV-Radiation">
-                <Tab2 />
-              </Tab>
-              <Tab tabStyle={{backgroundColor:"#F3BA36"}} activeTabStyle={{backgroundColor:"#F3BA36"}}
-              textStyle={{color:"black"}} activeTextStyle={{color:"white"}}  heading="Rainfall">
-                <Tab3 />
-              </Tab>
-            </Tabs>
-          </Container>
-        );
-      }
-    }
-const styles = StyleSheet.create({
-contaierStyle:{
-    backgroundColor:"#FAE5B6",
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return (
+      <Container tabStyle={styles.containerStyle} >
+        <Tabs tabBarUnderlineStyle={{backgroundColor:"white"}} >
+          <Tab tabStyle={{backgroundColor:"#F3BA36"}} activeTabStyle={{backgroundColor:"#F3BA36"}} 
+          textStyle={{color:"black"}} activeTextStyle={{color:"white"}} heading="Temperature">
+            <Tab1 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:"#F3BA36"}} activeTabStyle={{backgroundColor:"#F3BA36"}} 
+          textStyle={{color:"black"}} activeTextStyle={{color:"white"}} heading="UV Radiation">
+            <Tab2 />
+          </Tab>
+          <Tab tabStyle={{backgroundColor:"#F3BA36"}} activeTabStyle={{backgroundColor:"#F3BA36"}}
+          textStyle={{color:"black"}} activeTextStyle={{color:"white"}}  heading="Rainfall">
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
 }
-})
+
+const styles = StyleSheet.create({
+  containerStyle:{
+      backgroundColor:"#FAE5B6",
+  }
+});
